26c844cba339f726e47f0184a3314b45c9128daa
dgs99001 <dgs99001@proton.me>
2025-12-24 21:41:03 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -139,7 +139,7 @@ const apps = [
    { 
     id: 20, 
     name: "胜天国际", 
-    description: "官方最高点位招商，月底开业", 
+    description: "30号正式开业，可优先注册领取百万红包", 
     iconUrl: "https://pic1.imgdb.cn/item/6937c17f00233646958cd102.jpg",
     url: "https://wcwx.paradisemall.net/app/register.php?site_id=1050&topId=880722&selfPlanId=855049",
   },

d0682e73703b293e7d8648a491c4298cd2e492ea
dgs99001 <dgs99001@proton.me>
2026-01-15 11:44:27 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const apps = [
      {
     id: 1,
     name: "新时代",
-    description: "新台大水",
+    description: "注册即送88",
     iconUrl: "https://i.postimg.cc/t4SjcBzw/xin-shi-dai.jpg",
     url: "https://derear.fnbpsw.com/app/register.php?site_id=1059&topId=82351&selfPlanId=144338",
   },
